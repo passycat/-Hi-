5a03895e65b2452d32100a214daab773ed4d7b89
moggycat <291744610@qq.com>
2020-04-20 13:12:56 +0800

    update20.

--- pages/functions/ui/ui.js
+++ pages/functions/ui/ui.js
@@ -1,13 +1,66 @@
 // pages/functions/ui/ui.js
+const db = wx.cloud.database().collection('UItest')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    //图片回调参数
+    imgList: [],
+    attachments: [],
+    imageArray: [],
   },
+  ChooseImage() {
+    wx.chooseImage({
+      count: 4, //默认9
+      sizeType: ['original', 'compressed'], 
+      sourceType: ['album'], 
+      success: (res) => {
+        if (this.data.imgList.length != 0) {
+          this.setData({
+            attachments: this.data.imgList.concat(res.tempFilePaths)
+          })
 
+        } else {
+          this.setData({
+            attachments: res.tempFilePaths
+
+          })
+          console.log('选择成功', res.tempFilePaths)
+         /*图片选择成功后上传至云存储和云数据库记录图片路劲*/
+          this.data.imageArray.map(item => {
+            attachments.push(item.uploadResult.key)
+          })
+          let attachments = this.data.attachments;
+          console.log(this.data.attachments)
+          db.add({
+            data:{
+              'attachments': attachments
+            },
+            
+             success(res) {
+              console.log('提交结果', res)
+              var path=res._id+'.png'
+              wx.cloud.uploadFile({
+                
+                cloudPath: path,
+                filePath: attachments[0],
+                success(res) {
+                  console.log('上传成功', res)
+
+                },
+                 fail(res) {
+                  console.log('上传失败', res)
+                 }
+              })
+            }
+          })
+         
+        }
+      }
+    });
+  },
   /**
    * 生命周期函数--监听页面加载
    */
